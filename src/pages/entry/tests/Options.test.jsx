8064857f2b4043d7296c185107ce5e84fd2697e8
Cláudio Pedro Mindouro <claudiopedrom@gmail.com>
2024-01-15 22:17:05 +0100

    test(options): deal with async images - await findBy

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,5 +1,4 @@
-import { render, screen } from "../../../test-utils/testing-library-utils";
-import { userEvent } from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
